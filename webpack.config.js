b570d84d1e239b8225cb62543931bc561ed630e3
berta <berta.besteiro@gmail.com>
2016-06-29 11:17:18 +0200

    instalación de html-webpack-plugin para que genere el index.html dinámicamente

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: path.resolve(__dirname, 'app/main.js'),
   output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'bundle.js',
-  }
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hola Mundo'
+    })
+  ]
 };
